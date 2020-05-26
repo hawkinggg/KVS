b13e9923b9f78304f2bc532481c669a80e760f06
hawkinggg <690302166@qq.com>
2020-05-26 15:06:44 +0800

    project-1 part-6 pass

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,27 +1,29 @@
+use clap::{App, AppSettings, Arg, SubCommand};
 use std::process;
-use clap::{Arg, App, SubCommand, AppSettings};
 
 fn main() {
     let matches = App::new(env!("CARGO_PKG_NAME"))
-                    .version(env!("CARGO_PKG_VERSION"))
-                    .author(env!("CARGO_PKG_AUTHORS"))
-                    .about(env!("CARGO_PKG_DESCRIPTION"))
-                    .setting(AppSettings::SubcommandRequiredElseHelp)
-                    .subcommand(SubCommand::with_name("set")
-                        .about("set value for key, usage: kvs set key1 value1")
-                        .arg(Arg::with_name("key")
-                            .index(1))
-                        .arg(Arg::with_name("value")
-                            .index(2)))
-                    .subcommand(SubCommand::with_name("get")
-                        .about("get value by key, usage: kvs get key1")
-                        .arg(Arg::with_name("key")
-                            .index(1)))
-                    .subcommand(SubCommand::with_name("rm")
-                        .about("remove value by key, usage: kvs rm key1")
-                        .arg(Arg::with_name("key")
-                            .index(1)))
-                    .get_matches();
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("set value for key, usage: kvs set key1 value1")
+                .arg(Arg::with_name("key").index(1))
+                .arg(Arg::with_name("value").index(2)),
+        )
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("get value by key, usage: kvs get key1")
+                .arg(Arg::with_name("key").index(1)),
+        )
+        .subcommand(
+            SubCommand::with_name("rm")
+                .about("remove value by key, usage: kvs rm key1")
+                .arg(Arg::with_name("key").index(1)),
+        )
+        .get_matches();
 
     if matches.is_present("set") {
         eprintln!("sorry, unimplemented.");
@@ -38,4 +40,3 @@ fn main() {
         process::exit(1);
     }
 }
-    
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! # KVStore
-//! 
+//!
 //! A key/value database store string pair in-memory
 
 #![deny(missing_docs)]
@@ -9,19 +9,21 @@ use std::collections::HashMap;
 /// the kernal struct of KVStore
 #[derive(Default)]
 pub struct KvStore {
-    store: HashMap<String,String>,
+    store: HashMap<String, String>,
 }
 
 impl KvStore {
     /// Create a KvStore to store key/value pair.
     pub fn new() -> KvStore {
-        KvStore{store: HashMap::new()}
+        KvStore {
+            store: HashMap::new(),
+        }
     }
 
     /// Insert or update a key's value.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// let mut store = kvs::KvStore::new();
     /// store.set("foo".to_string(), "bar".to_string());
@@ -31,16 +33,16 @@ impl KvStore {
     }
 
     /// Get value by key, get None if key not exist.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// let mut store = kvs::KvStore::new();
     /// store.set("foo".to_string(), "bar".to_string());
     /// assert_eq!(store.get("foo".to_string()).unwrap(), "bar".to_string());
     /// assert_eq!(store.get("goo".to_string()), None);
     /// ```
-    pub fn get(& self, key: String) -> Option<String> {
+    pub fn get(&self, key: String) -> Option<String> {
         match self.store.get(&key) {
             None => None,
             Some(v) => Some(v.clone()),
@@ -48,9 +50,9 @@ impl KvStore {
     }
 
     /// Remove a key/value pair, do nothing if key not exist.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// let mut store = kvs::KvStore::new();
     /// store.set("foo".to_string(), "bar".to_string());
